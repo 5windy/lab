c1fd3ffe6d8340a9284dae3d2ea29ec50aef19d2
KimJiyeon <KimJiyeon@192.168.10.254>
2024-03-15 10:02:02 +0900

    Add 1 to 50 Game : Collection List

---
+++ lab/src/lv08/Ex05.java
@@ -0,0 +1,24 @@
+package lv08;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+// 1 to 50
+
+class Card {
+
+}
+
+class Manager1to50 {
+	private final int SIZE = 5;
+	
+	private Vector<ArrayList<Card>> map;
+	private int[] data;	// {1, 2, 3 ... 50} -> shuffle
+	
+}
+
+class Ex05 {
+	public static void main(String[] args) {
+		
+	}
+}
